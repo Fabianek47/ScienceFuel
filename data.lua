8d15c1c102aab07acfd67e9778e6aea5d10edd52
Fabianek47 <fabianek@online-briefkasten.com>
2025-01-26 15:14:13 +0100

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,34 @@
+data.raw.tool["automation-science-pack"].fuel_value = "10MJ"
+data.raw.tool["automation-science-pack"].fuel_category = "chemical"
+data.raw.tool["automation-science-pack"].fuel_acceleration_multiplier = 1.1
+data.raw.tool["automation-science-pack"].fuel_top_speed_multiplier = 1
+
+data.raw.tool["logistic-science-pack"].fuel_value = "50MJ"
+data.raw.tool["logistic-science-pack"].fuel_category = "chemical"
+data.raw.tool["logistic-science-pack"].fuel_acceleration_multiplier = 1.1
+data.raw.tool["logistic-science-pack"].fuel_top_speed_multiplier = 1
+
+data.raw.tool["chemical-science-pack"].fuel_value = "100MJ"
+data.raw.tool["chemical-science-pack"].fuel_category = "chemical"
+data.raw.tool["chemical-science-pack"].fuel_acceleration_multiplier = 1.3
+data.raw.tool["chemical-science-pack"].fuel_top_speed_multiplier = 1
+
+data.raw.tool["military-science-pack"].fuel_value = "100MJ"
+data.raw.tool["military-science-pack"].fuel_category = "chemical"
+data.raw.tool["military-science-pack"].fuel_acceleration_multiplier = 1.5
+data.raw.tool["military-science-pack"].fuel_top_speed_multiplier = 1.1
+
+data.raw.tool["production-science-pack"].fuel_value = "200MJ"
+data.raw.tool["production-science-pack"].fuel_category = "chemical"
+data.raw.tool["production-science-pack"].fuel_acceleration_multiplier = 1.7
+data.raw.tool["production-science-pack"].fuel_top_speed_multiplier = 1.1
+
+data.raw.tool["utility-science-pack"].fuel_value = "300MJ"
+data.raw.tool["utility-science-pack"].fuel_category = "chemical"
+data.raw.tool["utility-science-pack"].fuel_acceleration_multiplier = 1.8
+data.raw.tool["utility-science-pack"].fuel_top_speed_multiplier = 1.1
+
+data.raw.tool["space-science-pack"].fuel_value = "500MJ"
+data.raw.tool["space-science-pack"].fuel_category = "chemical"
+data.raw.tool["space-science-pack"].fuel_acceleration_multiplier = 2
+data.raw.tool["space-science-pack"].fuel_top_speed_multiplier = 1.1
